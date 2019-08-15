3df66d9ceac6a5ec0b5133827cbc46fba5fa7999
fab2s <fabrice.de.stefanis@gmail.com>
2019-08-15 09:58:51 +0200

    Initial upload

---
+++ README.md
@@ -0,0 +1,100 @@
+# Strings
+
+[![Build Status](https://travis-ci.org/fab2s/Strings.svg?branch=master)](https://travis-ci.org/fab2s/Strings) [![Latest Stable Version](https://poser.pugx.org/fab2s/strings/v/stable)](https://packagist.org/packages/fab2s/strings) [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/fab2s/strings/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/fab2s/Strings/?branch=master) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)](http://makeapullrequest.com) [![License](https://poser.pugx.org/fab2s/strings/license)](https://packagist.org/packages/fab2s/strings)
+
+A purely static String Helper to handle more advanced utf8 string manipulations
+
+## Installation
+
+`Strings` can be installed using composer:
+
+```
+composer require "fab2s/strings"
+```
+
+`Strings` is also included in [OpinHelper](https://github.com/fab2s/OpinHelpers) which packages several bellow "Swiss Army Knife" level Helpers covering some of the most annoying aspects of php programing, such as UTF8 string manipulation, high precision Mathematics or properly locking a file
+
+## Prerequisites
+
+As it requires `Utf8`, `Strings` requires [mb_string](https://php.net/mb_string), [ext-intl](https://php.net/intl) is auto detected and used when available for UTF-8 Normalization
+
+## In practice
+
+`Strings` implement some basic text manipulation function that can be pretty useful IRL
+
+
+### `filter(string $string):string` 
+
+Drops Zero Width white chars, normalizes EOL and Normalize UTF8 if [ext-intl](https://php.net/intl) is available
+
+### `singleWsIze(string $string, bool $normalize = false, bool $includeTabs = true):string` 
+
+Replace repeated white-spaces to a single one, preserve original white-spaces unless normalized (every white-spaces to ' '), with or without tabs (\t)
+
+### `singleLineIze(string $string):string` 
+
+Make string fit in one line by replacing EOLs and white-spaces to normalized single white-spaces
+
+### `dropZwWs(string $string):string` 
+
+Remove Zero Width white-spaces 
+
+### `normalizeWs(string $string, bool $includeTabs = true, int $maxConsecutive = null):string` 
+
+Normalize white-spaces to a single ` ` by default, include tabs by default
+
+### `normalizeEol($string, $maxConsecutive = null, $eol = null):string` 
+
+Normalize EOLs to a single LF by default
+
+### `normalizeText(string $text):string` 
+
+Return `trim`'d and `filter`'d $text 
+
+### `normalizeTitle(string $title):string` 
+
+Return `singleLineIze`'d and `normalizeText`'d $title
+
+### `normalizeName(string $name):string` 
+
+Return `ucword`'d and `normalizeTitle`'d $name (`"john \n\t doe  "` -> `"John Doe"`) 
+
+### `escape(string $string, int $flag = ENT_COMPAT, bool $hardEscape = true):string` 
+
+[htmlspecialchars()](https://php.net/htmlspecialchars) wrapper with UTF8 set as encoding
+
+### `softEscape(string $string, int $flag = ENT_COMPAT):string` 
+
+Shortcut for `escape(string $string, $flag, true)`
+
+### `unEscape(string $string, int $quoteStyle = ENT_COMPAT):string` 
+
+[htmlspecialchars_decode()](https://php.net/htmlspecialchars_decode) wrapper 
+
+### `convert(string $string, string $from = null, string $to = self::ENCODING):string` 
+
+Convert encoding to UTF8 by default. Basic $from encoding detection using `Strings::detectEncoding()`
+
+### `detectEncoding(string $string):string|null` 
+
+Detect encoding by checking `Utf8::isUf8()`, then trying with BOMs and ultimately fall back to [mb_detect_encoding()](https://php.net/mb_detect_encoding) with limited charsets first, then more internally in [mb_convert_encoding()](https://php.net/mb_convert_encoding) 
+
+### `secureCompare(string $test, string $reference):bool` 
+
+Perform a [Timing Attack](https://en.wikipedia.org/wiki/Timing_attack) safe string comparison (Truly constant operations comparison)
+
+### `contentHash(string $content):string` 
+
+Return a `sha256` hash of the $content prefixed with $content length. Indented to quickly and reliably detect $content updates.
+
+## Requirements
+
+`Strings` is tested against php 7.1, 7.2 and 7.3.
+
+## Contributing
+
+Contributions are welcome, do not hesitate to open issues and submit pull requests.
+
+## License
+
+`Strings` is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT)
---
+++ tests/StringsTest.php
@@ -0,0 +1,223 @@
+<?php
+
+/*
+ * This file is part of Strings
+ *     (c) Fabrice de Stefanis / https://github.com/fab2s/Strings
+ * This source file is licensed under the MIT license which you will
+ * find in the LICENSE file or at https://opensource.org/licenses/MIT
+ */
+
+namespace fab2s\Strings\Tests;
+
+use fab2s\Strings\Strings;
+
+/**
+ * Class StringsTest
+ */
+class StringsTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider filterData
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testFilter(string $input, string $expected)
+    {
+        $this->assertSame($expected, Strings::filter($input));
+    }
+
+    /**
+     * @dataProvider singleWsIzeData
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testSingleWsIze(string $input, string $expected)
+    {
+        $this->assertSame($expected, Strings::singleWsIze($input));
+    }
+
+    /**
+     * @dataProvider singleLineIzeData
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testSingleLineIze(string $input, string $expected)
+    {
+        $this->assertSame($expected, Strings::singleLineIze($input));
+    }
+
+    /**
+     * @dataProvider normalizeTitleData
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testNormalizeTitle(string $input, string $expected)
+    {
+        $this->assertSame($expected, Strings::normalizeTitle($input));
+    }
+
+    /**
+     * @dataProvider normalizeNameData
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testNormalizeName(string $input, string $expected)
+    {
+        $this->assertSame($expected, Strings::normalizeName($input));
+    }
+
+    /**
+     * @dataProvider normalizeWsData
+     *
+     * @param string   $input
+     * @param bool     $includeTabs
+     * @param int|null $maxConsecutive
+     * @param string   $expected
+     */
+    public function testNormalizeWs(string $input, bool $includeTabs, ?int $maxConsecutive, string $expected)
+    {
+        $this->assertSame($expected, Strings::normalizeWs($input, $includeTabs, $maxConsecutive));
+    }
+
+    /**
+     * @dataProvider normalizeEolData
+     *
+     * @param string   $input
+     * @param int|null $maxConsecutive
+     * @param string   $eol
+     * @param string   $expected
+     */
+    public function testNormalizeEol(string $input, ?int $maxConsecutive, string $eol, string $expected)
+    {
+        $this->assertSame($expected, Strings::normalizeEol($input, $maxConsecutive, $eol));
+    }
+
+    /**
+     * @return array
+     */
+    public function normalizeNameData(): array
+    {
+        return [
+            [
+                'input'          => "this is\r\r\n   one " . json_decode('"\u000B"') . 'text',
+                'expected'       => 'This Is One Text',
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function normalizeTitleData(): array
+    {
+        return [
+            [
+                'input'          => "this is\r\r\none text \n\f\nwith \n\n\n tons of ws \f\tand LF's \r\nevery" . json_decode('"\u000B"') . 'where',
+                'expected'       => "This is one text with tons of ws and LF's every where",
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function singleLineIzeData(): array
+    {
+        return [
+            [
+                'input'          => "this is\r\r\none text \n\f\nwith \n\n\n tons of ws \f\tand LF's \r\nevery" . json_decode('"\u000B"') . 'where',
+                'expected'       => "this is one text  with   tons of ws  \tand LF's  every where",
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function normalizeEolData(): array
+    {
+        return [
+            [
+                'input'          => "this is\r\r\none text \n\f\nwith" . json_decode('"\u2028"') . json_decode('"\u2029"') . "tons of ws \f\tand LF's \r\nevery" . json_decode('"\u000B"') . 'where',
+                'maxConsecutive' => null,
+                'eol'            => "\n",
+                'expected'       => "this is\n\none text \n\n\nwith\n\ntons of ws \n\tand LF's \nevery\nwhere",
+            ],
+            [
+                'input'          => "this is\r\r\none text \n\f\nwith" . json_decode('"\u2028"') . json_decode('"\u2029"') . "tons of ws \f\tand LF's \r\nevery" . json_decode('"\u000B"') . 'where',
+                'maxConsecutive' => 1,
+                'eol'            => "\n",
+                'expected'       => "this is\none text \nwith\ntons of ws \n\tand LF's \nevery\nwhere",
+            ],
+            [
+                'input'          => "this is\r\r\n\rone text \n\r\n\nwith" . json_decode('"\u2028"') . json_decode('"\u2029"') . "tons of ws \f\tand LF's \r\nevery" . json_decode('"\u000B"') . 'where',
+                'maxConsecutive' => 2,
+                'eol'            => "\n",
+                'expected'       => "this is\n\none text \n\nwith\n\ntons of ws \n\tand LF's \nevery\nwhere",
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function normalizeWsData(): array
+    {
+        return [
+            [
+                'input'          => "this is                     one   text \nwith " . json_decode('"\u2009"') . json_decode('"\u2009"') . "tons of ws \t\tand LF's \r\neverywhere",
+                'includeTabs'    => true,
+                'maxConsecutive' => 1,
+                'expected'       => "this is one text \nwith tons of ws and LF's \r\neverywhere",
+            ],
+            [
+                'input'          => "this is     one more   text \nwith tons of ws \t\tand LF's \r\neverywhere",
+                'includeTabs'    => false,
+                'maxConsecutive' => 1,
+                'expected'       => "this is one more text \nwith tons of ws \t\tand LF's \r\neverywhere",
+            ],
+            [
+                'input'          => "this is     another   text \nwith " . json_decode('"\u2009"') . json_decode('"\u2009"') . "tons of ws \t\tand LF's \r\neverywhere",
+                'includeTabs'    => false,
+                'maxConsecutive' => 2,
+                'expected'       => "this is  another  text \nwith  tons of ws \t\tand LF's \r\neverywhere",
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function singleWsIzeData(): array
+    {
+        return [
+            // input, expected
+            [
+                'input'    => "this is   a   text \nwith tons of ws \t\tand LF's \r\neverywhere",
+                'expected' => "this is a text \nwith tons of ws \tand LF's \r\neverywhere",
+            ],
+            [
+                'input'    => "this is a   text \f\nwith" . json_decode('"\u2009"') . json_decode('"\u2009"') . "tons of ws \t  \tand LF's \r\neverywhere",
+                'expected' => "this is a text \f\nwith" . json_decode('"\u2009"') . "tons of ws \t \tand LF's \r\neverywhere",
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function filterData(): array
+    {
+        return [
+            // input, expected
+            ["this is a \x00text \n\nwith \rws \t\tand LF's \r\neverywh\0ere", "this is a text \n\nwith \nws \t\tand LF's \neverywhere"],
+            ["this is another text \0\nwith \r\rws \t\tand LF's \r\neverywhere", "this is another text \nwith \n\nws \t\tand LF's \neverywhere"],
+            ['this is yet an' . json_decode('"\uFEFF"') . "other text \0\nwith \n\rws \t\tand LF's \r\nevery" . json_decode('"\u200B"') . 'where !!ù€@à!!', "this is yet another text \nwith \n\nws \t\tand LF's \neverywhere !!ù€@à!!"],
+        ];
+    }
+}
